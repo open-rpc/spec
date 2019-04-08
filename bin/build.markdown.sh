141d4e3c6f0b0b9964d89e2e336415c874bf7e6c
Zachary Belford <belfordz66@gmail.com>
2019-04-08 12:58:48 -0700

    fix: release version templated

--- bin/build.markdown.sh
+++ bin/build.markdown.sh
@@ -4,16 +4,26 @@ const {promisify} = require("util");
 const downloadReleases = require('@etclabscore/dl-github-releases');
 const fs = require("fs");
 const fsx = require("fs-extra");
+var toc = require('markdown-toc');
 const mkdir = promisify(fs.mkdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 const execFile = promisify(require('child_process').execFile);
 const copyFile = promisify(fs.copyFile);
+
+
+const specVersion = require("./get-version.js")
+
+const replaceVersionComments = s => s.replace("<!-- version -->", `Version ${specVersion}`);
 
 const build = async () => {
   const buildDir = "./build/markdown/";
   await fsx.ensureDir(buildDir);
   await fsx.emptyDir(buildDir);
-  await copyFile(`./spec.md`, `${buildDir}/spec.md`);
-  await execFile("./node_modules/.bin/markdown-toc", ["-i",`./${buildDir}/spec.md`]);
+  const specContent = await readFile("./spec.md", "utf8");
+  const withToc = toc(specContent).content;
+  const withVersion = replaceVersionComments(withToc);
+  await writeFile(`${buildDir}/spec.md`, withVersion)
 
   console.log("building markdown complete. Markdown is ready to be released!");
 
